5ebb63977f5b3fbf12703d7e72304ffa6a084fe9
summer2050 <lichunyan2015@gmail.com>
2025-06-19 13:53:06 +0800

    Update config.js

--- themes/example/config.js
+++ themes/example/config.js
@@ -3,14 +3,14 @@
  */
 const CONFIG = {
   // 菜单配置
-  EXAMPLE_MENU_CATEGORY: true, // 显示分类
-  EXAMPLE_MENU_TAG: true, // 显示标签
+  EXAMPLE_MENU_CATEGORY: false, // 显示分类
+  EXAMPLE_MENU_TAG: false, // 显示标签
   EXAMPLE_MENU_ARCHIVE: true, // 显示归档
   EXAMPLE_MENU_SEARCH: true, // 显示搜索
 
-  EXAMPLE_POST_LIST_COVER: true, // 列表显示文章封面
+  EXAMPLE_POST_LIST_COVER: false, // 列表显示文章封面
 
-  EXAMPLE_TITLE_IMAGE: false, // 标题栏，是否背景图片
+  EXAMPLE_TITLE_IMAGE: true, // 标题栏，是否背景图片
 
   // 文章页面布局
   EXAMPLE_ARTICLE_LAYOUT_VERTICAL: false, // 文章详情，左右布局改为上下布局
