6874014fbb9b4961d2b5e8e7fa3604b9ce5d4ac7
summer2050 <lichunyan2015@gmail.com>
2025-08-08 12:54:18 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -17,11 +17,11 @@ const CONFIG = {
   MATERY_MENU_ARCHIVE: true, // 显示归档
   MATERY_MENU_SEARCH: true, // 显示搜索
 
-  MATERY_POST_LIST_COVER: true, // 文章封面
+  MATERY_POST_LIST_COVER: false, // 文章封面
   MATERY_POST_LIST_SUMMARY: true, // 文章摘要
-  MATERY_POST_LIST_PREVIEW: true, // 读取文章预览
+  MATERY_POST_LIST_PREVIEW: false, // 读取文章预览
 
-  MATERY_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
+  MATERY_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
   MATERY_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
   MATERY_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
   MATERY_ARTICLE_RECOMMEND: false, // 文章关联推荐
