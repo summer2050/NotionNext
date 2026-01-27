8a1c23dfe1a4320a8ea28221302a882a7ec8355d
summer2050 <lichunyan2015@gmail.com>
2026-01-27 18:04:15 +0800

    Update BlogItem.js

--- themes/typography/components/BlogItem.js
+++ themes/typography/components/BlogItem.js
@@ -37,7 +37,7 @@ export const BlogItem = props => {
           <h2 className='mb-2'>
             <SmartLink
               href={post.href}
-              className='text-xl underline decoration-2 font-bold text-[var(--primary-color)] dark:text-white dark:hover:bg-white dark:hover:text-[var(--primary-color)]  duration-200 transition-all rounded-sm'>
+              className='text-xl decoration-2 font-bold text-[var(--primary-color)] dark:text-white dark:hover:bg-white dark:hover:text-[var(--primary-color)]  duration-200 transition-all rounded-sm'>
               {siteConfig('POST_TITLE_ICON') && (
                 <NotionIcon icon={post.pageIcon} />
               )}
